dd622a40e0e97e41475b5ab6003371b1024e69b8
mbulic97 <mirnesbulic32@gmail.com>
2023-12-08 18:37:24 +0100

    Login register act.frag.

---
+++ app/src/main/java/com/example/ecommerceappmirnes/activities/LoginRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ecommerceappmirnes.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ecommerceappmirnes.R
+
+class LoginRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.actitivty_login_register)
+    }
+}

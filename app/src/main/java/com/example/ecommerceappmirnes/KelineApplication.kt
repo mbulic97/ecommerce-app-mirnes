85f6621ed5841dae184a9a09c44b50cd18153896
mbulic97 <mirnesbulic32@gmail.com>
2023-12-15 23:25:29 +0100

    Login register act.frag.

---
+++ app/src/main/java/com/example/ecommerceappmirnes/KelineApplication.kt
@@ -0,0 +1,8 @@
+package com.example.ecommerceappmirnes
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class KelineApplication: Application() {
+}
--- app/src/main/java/com/example/ecommerceappmirnes/fragment/loginRegister/RegisterFragment.kt
+++ app/src/main/java/com/example/ecommerceappmirnes/fragment/loginRegister/RegisterFragment.kt
@@ -1,7 +1,30 @@
 package com.example.ecommerceappmirnes.fragment.loginRegister
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.ecommerceappmirnes.R
+import com.example.ecommerceappmirnes.databinding.FragmentRegisterBinding
+import com.example.ecommerceappmirnes.viewmodel.RegisterViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-class RegisterFragment: Fragment(R.layout.fragment_register) {
+@AndroidEntryPoint
+class RegisterFragment: Fragment() {
+ private lateinit var binding: FragmentRegisterBinding
+ private val viewModel by viewModels <RegisterViewModel>()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentRegisterBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }

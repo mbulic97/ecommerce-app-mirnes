035aef3c59409a7a4e2f59bbb983294fb6d80cd3
mbulic97 <mirnesbulic32@gmail.com>
2023-12-19 23:57:17 +0100

    Login and navigations

--- app/src/main/java/com/example/ecommerceappmirnes/fragment/loginRegister/RegisterFragment.kt
+++ app/src/main/java/com/example/ecommerceappmirnes/fragment/loginRegister/RegisterFragment.kt
@@ -8,6 +8,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.example.ecommerceappmirnes.R
 import com.example.ecommerceappmirnes.data.User
 import com.example.ecommerceappmirnes.databinding.FragmentRegisterBinding
 import com.example.ecommerceappmirnes.util.RegisterValidation
@@ -34,6 +36,10 @@ class RegisterFragment: Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.tvDoYouHaveAccount.setOnClickListener {
+            findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+        }
         binding.apply {
             buttonRegisterRegister.setOnClickListener {
                 val user= User(
